baea7a1a0768c4fab1976a019751255ec84826ba
Keino Chichester <keino.chichester@gmail.com>
2023-03-27 22:10:05 -0400

    bad news, i removed all original backend code. good news. we are able to register users.

--- server/app.py
+++ server/app.py
@@ -1,98 +1,151 @@
 #!/usr/bin/env python3
 
-# Standard library imports
+from flask import request, session
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
 
-# Remote library imports
-from flask import Flask, render_template, url_for, redirect, request, flash
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from faker import Faker
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///event_space.db'
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-class UserRole(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
-class Customer(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-    role_id = db.Column(db.Integer, db.ForeignKey("user_role.id"), nullable=False)
-    role = db.relationship("UserRole", backref="users")
-    event_spaces = db.relationship("EventSpace", backref="owner", lazy=True)
-    reviews = db.relationship("Review", backref="user", lazy=True)
-class Review(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.Text, nullable=False)
-    rating = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("customer.id"), nullable=False)
-    user = db.relationship("Customer", backref="reviews")
-    event_space_id = db.Column(db.Integer, db.ForeignKey("event_space.id"), nullable=False)
-    event_space = db.relationship("EventSpace", backref="reviews")
-class Property(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    location = db.Column(db.String(120), nullable=False)
-    price = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.String(255), nullable=True)
-    owner_id = db.Column(db.Integer, db.ForeignKey("customer.id"), nullable=False)
-class EventSpace(Property):
-    __tablename__ = "event_space"
-    id = db.Column(db.Integer, primary_key=True)
-    capacity = db.Column(db.Integer, nullable=False)
-    amenities = db.Column(db.String(255), nullable=True)
-    owner_id = db.Column(db.Integer, db.ForeignKey("customer.id"), nullable=False)
-    reviews = db.relationship("Review", backref="event_space", lazy=True)
-# Define your routes here
-@app.route('/')
-def home():
-    # Your home page logic
-    pass
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    # Your signup logic
-    pass
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    # Your login logic
-    pass
-@app.route('/logout')
-@login_required
-def logout():
-    # Your logout logic
-    pass
-@login_manager.user_loader
-def load_user(user_id):
-    return Customer.query.get(int(user_id))
-def create_fake_data(num_customers=10, num_properties_per_customer=2):
-    fake = Faker()
-    for _ in range(num_customers):
-        # Create a fake customer
-        fake_customer = Customer(
-            username=fake.user_name(),
-            email=fake.email(),
-            password_hash=fake.password(length=12)
+from config import app, db, api
+from models import User, Recipe, db
+from config import db
+
+# db.create_all()
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+class Signup(Resource):
+    
+    def post(self):
+
+        request_json = request.get_json()
+
+        username = request_json.get('username')
+        password = request_json.get('password')
+        image_url = request_json.get('image_url')
+        bio = request_json.get('bio')
+
+        user = User(
+            username=username,
+            image_url=image_url,
+            bio=bio
         )
-        db.session.add(fake_customer)
-        db.session.commit()
-        for _ in range(num_properties_per_customer):
-            # Create a fake event space for the customer
-            fake_event_space = EventSpace(
-                name=fake.company(),
-                location=fake.address(),
-                price=fake.random_int(min=500, max=5000),
-                description=fake.text(max_nb_chars=200),
-                capacity=fake.random_int(min=10, max=500),
-                amenities=fake.text(max_nb_chars=100),
-                owner_id=fake_customer.id
-            )
-            db.session.add(fake_event_space)
+
+        # the setter will encrypt this
+        user.password_hash = password
+
+        print('first')
+
+        try:
+
+            print('here!')
+
+            db.session.add(user)
             db.session.commit()
-# Uncomment the following line to generate fake data
-# create_fake_data()
+
+            session['user_id'] = user.id
+
+            print(user.to_dict())
+
+            return user.to_dict(), 201
+
+        except IntegrityError:
+
+            print('no, here!')
+            
+            return {'error': '422 Unprocessable Entity'}, 422
+
+class CheckSession(Resource):
+    
+    def get(self):
+
+        if session.get('user_id'):
+
+            user = User.query.filter(User.id == session['user_id']).first()
+
+            return user.to_dict(), 200
+
+        return {'error': '401 Unauthorized'}, 401
+
+class Login(Resource):
+    
+    def post(self):
+
+        request_json = request.get_json()
+
+        username = request_json.get('username')
+        password = request_json.get('password')
+
+        user = User.query.filter(User.username == username).first()
+
+        if user:
+            if user.authenticate(password):
+
+                session['user_id'] = user.id
+                return user.to_dict(), 200
+
+        return {'error': '401 Unauthorized'}, 401
+
+class Logout(Resource):
+    
+    def delete(self):
+        
+        if session.get('user_id'):
+            
+            session['user_id'] = None
+            
+            return {}, 204
+        
+        return {'error': '401 Unauthorized'}, 401
+
+class RecipeIndex(Resource):
+
+    def get(self):
+
+        if session.get('user_id'):
+
+            user = User.query.filter(User.id == session['user_id']).first()
+
+            return [recipe.to_dict() for recipe in user.recipes], 200
+        
+        return {'error': '401 Unauthorized'}, 401
+        
+    def post(self):
+
+        if session.get('user_id'):
+
+            request_json = request.get_json()
+
+            title = request_json['title']
+            instructions = request_json['instructions']
+            minutes_to_complete = request_json['minutes_to_complete']
+
+            try:
+
+                recipe = Recipe(
+                    title=title,
+                    instructions=instructions,
+                    minutes_to_complete=minutes_to_complete,
+                    user_id=session['user_id'],
+                )
+
+                db.session.add(recipe)
+                db.session.commit()
+
+                return recipe.to_dict(), 201
+
+            except IntegrityError:
+
+                return {'error': '422 Unprocessable Entity'}, 422
+
+        return {'error': '401 Unauthorized'}, 401
+
+api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5555, debug=True)
