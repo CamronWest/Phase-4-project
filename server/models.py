b6a114333f8c88433a1547b069cc9063c9ec794c
Forrest Jones <forrestjones2010@gmail.com>
2023-03-28 12:20:06 -0500

    FJ push after fleshing out backend with models user owner and property. can not seed tables properly

--- server/models.py
+++ server/models.py
@@ -1,5 +1,35 @@
 from sqlalchemy_serializer import SerializerMixin
-
 from config import db
+from sqlalchemy import Table, Column, Integer, ForeignKey
 
-# Models go here!
+# Define the association table
+property_user_association_table = db.Table('property_user_association',
+    db.Column('property_id', db.Integer, db.ForeignKey('property.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+class User(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    # Define the many-to-many relationship with Property
+    owned_properties = db.relationship('Property', secondary=property_user_association_table, back_populates='owners')
+
+class Owner(User):
+    __tablename__ = 'owner'
+    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    owned_properties = db.relationship("Property", backref="owner", lazy=True)
+
+class Property(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+
+    # Define the many-to-many relationship with User
+    owners = db.relationship('User', secondary=property_user_association_table, back_populates='owned_properties')
+
+    owner_id = db.Column(db.Integer, db.ForeignKey("owner.id"), nullable=False)
--- server/seed.py
+++ server/seed.py
@@ -1,17 +1,28 @@
-#!/usr/bin/env python3
-
-
-from random import randint, choice as rc
-
-
 from faker import Faker
+from app import db
+from models import User, Owner, Property
 
-# Local imports
-from app import app
-from models import db
-
-if __name__ == '__main__':
+def seed_data(num_users=10, num_properties_per_owner=2):
     fake = Faker()
-    with app.app_context():
-        print("Starting seed...")
-        # Seed code goes here!
+    for _ in range(num_users):
+        # Create a fake owner
+        fake_owner = Owner(
+            username=fake.user_name(),
+            email=fake.email(),
+            password_hash=fake.password()
+        )
+        db.session.add(fake_owner)
+        db.session.commit()
+        
+        for _ in range(num_properties_per_owner):
+            # Create a fake property
+            fake_property = Property(
+                name=fake.word(),
+                location=fake.address(),
+                price=fake.random_int(min=500, max=5000),
+                description=fake.text(max_nb_chars=200)
+            )
+            fake_property.owners.append(fake_owner)
+            db.session.add(fake_property)
+            db.session.commit()
+    print(f"Successfully seeded {num_users} users and {num_properties_per_owner} properties per user.")
