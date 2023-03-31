8e018d43acfa1e9a0b7ee1fc8c3f4886c113241d
Camron <cawe219@gmail.com>
2023-03-31 02:36:55 -0400

    updated dashboard.

--- client/src/components/Home-Access/Dashboard/Dashboard.jsx
+++ client/src/components/Home-Access/Dashboard/Dashboard.jsx
@@ -1,64 +1,21 @@
 import React, {useState, useEffect} from "react"
 import Nav from "../Home/Nav.jsx"
+import PropertyList from "./PropertyList.jsx";
 
-function Dashboard({ userData }) {
-    const [formState, setFormState] = useState(userData);
+function Dashboard({ userId }) {
+    const [properties, setProperties] = useState([]);
   
-    function handleInputChange(event) {
-      const target = event.target;
-      const value = target.value;
-      const username = target.username;
+    useEffect(() => {
+      // Fetch the user's list of properties from the server and update the state
+      fetch(`/api/users/${userId}/properties`)
+        .then((response) => response.json())
+        .then((data) => setProperties(data));
+    }, [userId]);
   
-      setFormState({
-        ...formState,
-        [username]: value
-      });
-    }
-  
-    function handleSubmit(event) {
-      event.preventDefault();
-      
-      fetch('/updateUser', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formState)
-      })
-        .then(response => response.json())
-        .then(data => {
-          
-          console.log(data);
-        })
-        .catch(error => {
-          
-          console.error(error);
-        });
-    }
-  
-    return (
-      <form onSubmit={handleSubmit}>
-        <label>
-          username:
-          <input
-            type="text"
-            username="username"
-            value={formState.username}
-            onChange={handleInputChange} />
-        </label>
-        <label>
-          Email:
-          <input
-            type="email"
-            username="email"
-            value={formState.email}
-            onChange={handleInputChange} />
-        </label>
-        <button onClick={() => setFormState({ ...formState, username: 'New username' })}>Update username</button>
-        <button onClick={() => setFormState({ ...formState, email: 'new-email@example.com' })}>Update Email</button>
-        <button type="submit">Submit</button>
-      </form>
-    );
+    return 
+    <div>
+B YHNG         <PropertyList properties={properties} />
+        </div>;
   }
 
 export default Dashboard
