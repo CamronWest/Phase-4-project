cbfac08883b166e29ad6ee49609f2ba96c58bb72
Camron <cawe219@gmail.com>
2023-03-31 08:59:48 -0400

    another update to dashboard.

--- client/src/components/Home-Access/Dashboard/Dashboard.jsx
+++ client/src/components/Home-Access/Dashboard/Dashboard.jsx
@@ -1,21 +1,39 @@
 import React, {useState, useEffect} from "react"
 import Nav from "../Home/Nav.jsx"
 import PropertyList from "./PropertyList.jsx";
+import * as Yup from "yup";
+import axios from "axios";
 
 function Dashboard({ userId }) {
     const [properties, setProperties] = useState([]);
-  
+
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const handleSubmit = async (values, { setSubmitting }) => {
+        try {
+            const response = await axios.post("/users", values);
+            console.log(response.data);
+            setSubmitting(false);
+        } catch (error) {
+            setErrorMessage(error.message);
+            setSubmitting(false);
+        }
+    };
+
     useEffect(() => {
       // Fetch the user's list of properties from the server and update the state
-      fetch(`/api/users/${userId}/properties`)
+      fetch(`/users/${userId}/properties`)
         .then((response) => response.json())
         .then((data) => setProperties(data));
     }, [userId]);
   
-    return 
+    
+    return (
     <div>
-B YHNG         <PropertyList properties={properties} />
-        </div>;
+        <PropertyList properties={properties} />
+        
+        </div>
+    )
   }
 
 export default Dashboard
